7a44d5a18ac1e79f196f07ae0a32c96d1161f4be
Laurent <lmilne360@gmail.com>
2019-01-06 18:07:49 -0600

    basic reducer setup

---
+++ src/reducers/tutorial.reducer.ts
@@ -0,0 +1,19 @@
+import * as TutorialActions from './../actions/tutorial.actions';
+import { Tutorial } from 'src/models/tutorial.model';
+
+
+const initialState: Tutorial = {
+    name: 'Initial Tutorial',
+    url: 'google.com'
+};
+
+export function reducer(state: Tutorial[]  = [initialState], action: TutorialActions.Actions ) {
+  switch (action.type) {
+    case TutorialActions.ADD_TUTORIAL: {
+      return [...state, action.payload];
+    }
+    default: {
+      return state;
+    }
+  }
+}
